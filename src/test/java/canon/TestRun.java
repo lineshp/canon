050137a4d7897ce1ba1dc402d9f1619c3f80ad09
lines <lines@192.168.108.1>
2020-03-05 23:54:26 +1030

    updating junit

--- src/test/java/canon/TestRun.java
+++ src/test/java/canon/TestRun.java
@@ -1,8 +1,9 @@
 package canon;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 
 public class TestRun {
 
