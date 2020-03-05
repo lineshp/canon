b9b41aaa59c774b1b06bbdf9a0e84d8db999a9db
lines <lines@192.168.108.1>
2020-03-05 23:29:31 +1030

    adding unit test

---
+++ src/test/java/canon/TestRun.java
@@ -0,0 +1,14 @@
+package canon;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestRun {
+
+	
+	@Test
+	public void testAddition() {
+		assertTrue(5>7);
+	}
+}
