06039e406693ac4ac7fe7ac07b74f9b40d4957c7
lines <lines@192.168.108.1>
2020-03-06 00:02:02 +1030

    add test case

--- src/test/java/canon/TestRun.java
+++ src/test/java/canon/TestRun.java
@@ -1,12 +1,10 @@
 package canon;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class TestRun {
-
 	
 	@Test
 	public void testAddition() {
